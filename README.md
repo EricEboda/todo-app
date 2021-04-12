7ef72b1e0267058a71b6499514375995cb5cf7c9
Eric Eboda <ericjeb3@gmail.com>
2021-04-12 11:35:06 +0100

    Todo CRUD App - Updated

---
+++ README.md
@@ -0,0 +1,8 @@
+# TODO CRUD APP
+
+A simple app which allows you to make a todo list
+
+  * Create new tasks
+  * Check tasks as completed
+  * Edit existing tasks
+  * Remove tasks
---
+++ lib/app_web/views/item_view.ex
@@ -0,0 +1,23 @@
+defmodule AppWeb.ItemView do
+  use AppWeb, :view
+
+  def complete(item) do
+    case item.status do
+      1 -> "completed"
+      _ -> "" # empty string means empty class so no style applied
+    end
+  end
+
+  # add "checked" to input if item.status=1
+  def checked(item) do
+    case item.status do
+      1 -> "checked"
+      _ -> "" # empty string means empty class so no style applied
+    end
+  end
+
+  # returns integer value of items where item.status == 0 (not "done")
+  def remaining_items(items) do
+    Enum.filter(items, fn i -> i.status == 0 end) |> Enum.count
+  end
+end
